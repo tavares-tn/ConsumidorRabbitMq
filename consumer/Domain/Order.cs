f71f98b1e3bc98690c748d92d962fce06f80a054
Thales Tavares <tavares@redes.ufsm.br>
2022-09-20 22:47:01 -0300

    código exemplo consumidor fila

---
+++ consumer/Domain/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consumer.Domain
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public string ItemName { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ consumer/Program.cs
@@ -0,0 +1,65 @@
+﻿using consumer.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace consumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria conexão com RabbitMQ
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = ConnectionFactory.DefaultUser,
+                Password = ConnectionFactory.DefaultPass,
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+
+            // cria conexão
+            var connection = factory.CreateConnection();
+
+            // cria a canal de comunicação com RabbitMQ
+            var channel = connection.CreateModel();
+
+
+            //Cria a fila caso não exista
+            channel.QueueDeclare(queue: "orderQueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    var order = JsonSerializer.Deserialize<Order>(message);
+
+                    Console.WriteLine($"OrderNumber: {order.OrderNumber}");
+                    Console.WriteLine($"OrderName: {order.ItemName}");
+                    Console.WriteLine($"Price: {order.Price}");
+                }
+                catch (Exception ex)
+                {
+                    // Logger
+                    channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+            
+
+            channel.BasicConsume(queue: "orderQueue",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+        }
+    }
+}
